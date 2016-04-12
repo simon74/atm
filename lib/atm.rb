621193b203a720abac771a6c16860895df6f1813
simon74 <simon@hemph.se>
2016-04-12 15:19:22 +0200

    sucessful witdraw

--- lib/atm.rb
+++ lib/atm.rb
@@ -1,17 +1,28 @@
 class Atm
-	attr_accessor :balance
+  attr_accessor :balance 
+  DEFAULT_BILLS = [20, 10, 5]
 
-	def initialize
-		@balance = 1000
-	end
+  def initialize
+    @balance = 1000
+  end
 
-	def withdraw(amount)
-		if amount % 5 == 0
-			@balance = @balance - amount	
-			# true
-			return ({status: true, bills: [10, 5]})
-		else
-			false
-		end
-	end
+  def withdraw(amount)
+    if amount % 5 == 0
+      @balance = @balance - amount 
+      { status: true, message: 'success', date: Date.today, amount: amount, bills: add_bills(amount)}
+    else
+      false
+    end
+  end
+
+def add_bills(amount)
+  output = []
+  DEFAULT_BILLS.each do |bill|
+    while amount - bill >= 0
+      amount = amount - bill 
+      output << bill
+    end
+    output
+  end
+ end
 end
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,21 +1,25 @@
 require './lib/atm.rb'
 
 describe Atm do
-	it 'has 1000$ on int.' do
+	it 'has 1000$ on initializing.' do
 		expect(subject.balance).to eq 1000
 	end
-
+  
 	it 'balance is reduced at withdraw' do
 		subject.withdraw 5
 		expect(subject.balance).to eq 995
 	end
 
-	it 'expect withdraw of 15 to output bills array' do
-		expect(subject.withdraw(15)).to eq({status: true, bills: [10, 5]})
+	xit 'expect withdraw of 15 to output bills array' do
+		expect(subject.add_bills(15)).to eq({status: true, bills: [10, 5]})
 	end
 
-	it 'reject if amount is not devisiable by 5' do
+	it 'reject if amount is not divisible by 5' do
 		expect(subject.withdraw 6).to eq false
 	end
 
+	it 'returns at succeful withdraw' do
+		expected_output = { status: true, message: 'success', date: Date.today, amount: 35, bills: [20,10,5]}
+		expect(subject.withdraw 35).to eq expected_output
+	end
 end
