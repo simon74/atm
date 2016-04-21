45d5850139d5cbf331bd53f70a7fddfe4b181bca
simon74 <simon@hemph.se>
2016-04-21 12:09:42 +0200

    removed unused  test

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -40,22 +40,18 @@ describe Atm do
    		expect(subject.withdraw(150, 1234, account)).to eq expected_output
   end
 
-	xit 'expect withdraw of 15 to output bills array' do
-		expect(subject.add_bills(15)).to eq({status: true, bills: [10, 5]})
-	end
+  it 'reject if amount is not divisible by 5' do
+    expected_output = { status: false, message: 'wrong amount', date: Date.today}
+    expect(subject.withdraw(6, 1234, account)).to eq expected_output
+  end
 
-	it 'reject if amount is not divisible by 5' do
-		expected_output = { status: false, message: 'wrong amount', date: Date.today}
-		expect(subject.withdraw(6, 1234, account)).to eq expected_output
-	end
+  it 'reject if pin is incorrect' do
+    expected_output = { status: false, message: 'wrong pin', date: Date.today}
+    expect(subject.withdraw(5, 1233, account)).to eq expected_output
+  end
 
-	it 'reject if pin is incorrect' do
-		expected_output = { status: false, message: 'wrong pin', date: Date.today}
-		expect(subject.withdraw(5, 1233, account)).to eq expected_output
-	end
-
-	it 'returns at successful withdraw' do
-		expected_output = { status: true, message: 'success', date: Date.today, amount: 35, bills: [20,10,5]}
-		expect(subject.withdraw(35, 1234, account)).to eq expected_output
-	end
+  it 'returns at successful withdraw' do
+    expected_output = { status: true, message: 'success', date: Date.today, amount: 35, bills: [20,10,5]}
+    expect(subject.withdraw(35, 1234, account)).to eq expected_output
+  end
 end
